a354052129bcc500ddcf2fdd54690636d6c99986
Ozires <ozires.cruz@gmail.com>
2020-05-03 16:39:14 -0700

    Validando a data

--- src/routes/appoitments.route.ts
+++ src/routes/appoitments.route.ts
@@ -1,17 +1,32 @@
 import { Router } from "express";
 import { uuid } from "uuidv4";
+import { startOfHour, parseISO, isEqual } from "date-fns";
 
 const appoitmentsRouter = Router();
 
-const appoitments: { id: string; provider: any; date: any }[] = [];
+interface Appoitment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appoitments: Appoitment[] = [];
 
 appoitmentsRouter.post("/", (request, response) => {
   const { provider, date } = request.body;
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppoitmentInSameDate = appoitments.find((appoitment) =>
+    isEqual(parsedDate, appoitment.date)
+  );
+
+  if (findAppoitmentInSameDate) {
+    return response.status(400).json({ error: "Day/Hour already booked!" });
+  }
 
   const appoitment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appoitments.push(appoitment);

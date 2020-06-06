10d1ae605287bfd2dad6036059ffdbb1b0cf0146
Ozires <ozires.cruz@gmail.com>
2020-06-06 12:02:58 -0700

    Atualizando avatar

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpateUserAvatarService';
 
 const usersRouter = Router();
 
@@ -27,9 +28,19 @@ usersRouter.post('/', async (request, response) => {
 
 usersRouter.patch(
   '/avatar',
+  ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+      return response.json({ user });
+    } catch (error) {
+      return response.json({ error });
+    }
   },
 );
 
